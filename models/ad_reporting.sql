5f01346f5759957994ad769e0bee7594e776af25
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-09-28 14:52:37 -0700

    revert seed stuff, some aaccount_ids are strings

--- models/ad_reporting.sql
+++ models/ad_reporting.sql
@@ -3,7 +3,7 @@
 with unioned as (
 
     {{ dbt_utils.union_relations(get_staging_files() 
-                                ) }}
+                                , column_override = {"account_id": "string"} if target.type in ['bigquery', 'string'] else {"account_id": "varchar"} ) }}
 
 )
 
