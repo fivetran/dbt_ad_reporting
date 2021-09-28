9bd4de6de510696bef42720df35421ade4beb5e3
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-09-28 14:15:18 -0700

    set all seed data

--- models/ad_reporting.sql
+++ models/ad_reporting.sql
@@ -2,8 +2,8 @@
 
 with unioned as (
 
-    {{ dbt_utils.union_relations(get_staging_files(), 
-                                column_override = {"account_id": "int64"} if target.type == 'bigquery' else {"account_id": "bigint"} ) }}
+    {{ dbt_utils.union_relations(get_staging_files() 
+                                ) }}
 
 )
 
