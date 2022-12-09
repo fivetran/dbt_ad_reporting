b3ca796f040d040deb613dd5c1b9d5f96e3ece3a
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2022-12-08 20:42:18 -0600

    update run_mode

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,3 +19,5 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
+dbt run --vars '{allow_ad_reporting_null_urls: true}' --target "$db" --full-refresh
+dbt test --target "$db"
