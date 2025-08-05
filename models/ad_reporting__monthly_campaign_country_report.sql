435930d813088a874ea8df7f0d065dd2fd4df4ad
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-08-05 12:52:00 -0700

    Enable/disable country & region reports

--- models/ad_reporting__monthly_campaign_country_report.sql
+++ models/ad_reporting__monthly_campaign_country_report.sql
@@ -1,14 +1,55 @@
 {% set include_list = [] %}
-{% do include_list.append('facebook_ads') if var('facebook_ads__using_demographics_country', false) %}
-{% do include_list.append('linkedin_ads') if var('linkedin_ads__using_geo', true) and var('linkedin_ads__using_monthly_ad_analytics_by_member_country', true) %}
-{% do include_list.append('microsoft_ads') if var('microsoft_ads__using_geographic_daily_report', false) %}
-{% do include_list.append('pinterest_ads') if var('pinterest__using_pin_promotion_targeting_report', true) and var('pinterest__using_targeting_geo', true) %}
-{% do include_list.append('reddit_ads') if var('reddit_ads__using_campaign_country_report', true) %}
-{% do include_list.append('snapchat_ads') if var('snapchat_ads__using_campaign_country_report', false) %}
-{% do include_list.append('tiktok_ads') if var('tiktok_ads__using_campaign_country_report', true) %}
-{% do include_list.append('twitter_ads') if var('twitter_ads__using_campaign_locations_report', false) %}
+{% set exclude_list = ['amazon_ads', 'apple_search_ads', 'google_ads'] %}
 
-{% set enabled_packages = get_enabled_packages(include=include_list)%}
+{% if var('facebook_ads__using_demographics_country', false) %}
+    {% do include_list.append('facebook_ads') %}
+{% else %}
+    {% do exclude_list.append('facebook_ads') %}
+{% endif %}
+
+{% if var('linkedin_ads__using_geo', true) and var('linkedin_ads__using_monthly_ad_analytics_by_member_country', true) %}
+    {% do include_list.append('linkedin_ads') %}
+{% else %}
+    {% do exclude_list.append('linkedin_ads') %}
+{% endif %}
+
+{% if var('microsoft_ads__using_geographic_daily_report', false) %}
+    {% do include_list.append('microsoft_ads') %}
+{% else %}
+    {% do exclude_list.append('microsoft_ads') %}
+{% endif %}
+
+{% if var('pinterest__using_pin_promotion_targeting_report', true) and var('pinterest__using_targeting_geo', true) %}
+    {% do include_list.append('pinterest_ads') %}
+{% else %}
+    {% do exclude_list.append('pinterest_ads') %}
+{% endif %}
+
+{% if var('reddit_ads__using_campaign_country_report', true) %}
+    {% do include_list.append('reddit_ads') %}
+{% else %}
+    {% do exclude_list.append('reddit_ads') %}
+{% endif %}
+
+{% if var('snapchat_ads__using_campaign_country_report', false) %}
+    {% do include_list.append('snapchat_ads') %}
+{% else %}
+    {% do exclude_list.append('snapchat_ads') %}
+{% endif %}
+
+{% if var('tiktok_ads__using_campaign_country_report', true) %}
+    {% do include_list.append('tiktok_ads') %}
+{% else %}
+    {% do exclude_list.append('tiktok_ads') %}
+{% endif %}
+
+{% if var('twitter_ads__using_campaign_locations_report', false) %}
+    {% do include_list.append('twitter_ads') %}
+{% else %}
+    {% do exclude_list.append('twitter_ads') %}
+{% endif %}
+
+{% set enabled_packages = get_enabled_packages(include=include_list) %}
 {{ config(enabled=is_enabled(enabled_packages)) }}
 
 with base as (
--- models/ad_reporting__monthly_campaign_region_report.sql
+++ models/ad_reporting__monthly_campaign_region_report.sql
@@ -1,12 +1,43 @@
 {% set include_list = [] %}
-{% do include_list.append('facebook_ads') if var('facebook_ads__using_demographics_region', false) %}
-{% do include_list.append('linkedin_ads') if var('linkedin_ads__using_geo', true) and var('linkedin_ads__using_monthly_ad_analytics_by_member_region', true) %}
-{% do include_list.append('microsoft_ads') if var('microsoft_ads__using_geographic_daily_report', false) %}
-{% do include_list.append('pinterest_ads') if var('pinterest__using_pin_promotion_targeting_report', true) and var('pinterest__using_targeting_geo_region', true) %}
-{% do include_list.append('snapchat_ads') if var('snapchat_ads__using_campaign_region_report', false) %}
-{% do include_list.append('twitter_ads') if var('twitter_ads__using_campaign_regions_report', false) %}
+{% set exclude_list = ['amazon_ads', 'apple_search_ads', 'google_ads', 'reddit_ads', 'tiktok_ads'] %}
 
-{% set enabled_packages = get_enabled_packages(include=include_list)%}
+{% if var('facebook_ads__using_demographics_region', false) %}
+    {% do include_list.append('facebook_ads') %}
+{% else %}
+    {% do exclude_list.append('facebook_ads') %}
+{% endif %}
+
+{% if var('linkedin_ads__using_geo', true) and var('linkedin_ads__using_monthly_ad_analytics_by_member_region', true) %}
+    {% do include_list.append('linkedin_ads') %}
+{% else %}
+    {% do exclude_list.append('linkedin_ads') %}
+{% endif %}
+
+{% if var('microsoft_ads__using_geographic_daily_report', false) %}
+    {% do include_list.append('microsoft_ads') %}
+{% else %}
+    {% do exclude_list.append('microsoft_ads') %}
+{% endif %}
+
+{% if var('pinterest__using_pin_promotion_targeting_report', true) and var('pinterest__using_targeting_geo_region', true) %}
+    {% do include_list.append('pinterest_ads') %}
+{% else %}
+    {% do exclude_list.append('pinterest_ads') %}
+{% endif %}
+
+{% if var('snapchat_ads__using_campaign_region_report', false) %}
+    {% do include_list.append('snapchat_ads') %}
+{% else %}
+    {% do exclude_list.append('snapchat_ads') %}
+{% endif %}
+
+{% if var('twitter_ads__using_campaign_regions_report', false) %}
+    {% do include_list.append('twitter_ads') %}
+{% else %}
+    {% do exclude_list.append('twitter_ads') %}
+{% endif %}
+
+{% set enabled_packages = get_enabled_packages(include=include_list) %}
 {{ config(enabled=is_enabled(enabled_packages)) }}
 
 with base as (
