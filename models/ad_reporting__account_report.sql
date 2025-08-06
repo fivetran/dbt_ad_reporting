bc05c29a97ad43fce4a27bae108469a15fa9e494
atulkr-mbm <atul.krishnan@mediabymother.com>
2025-08-06 13:35:25 +0530

    added conversions_value for YT

--- models/ad_reporting__account_report.sql
+++ models/ad_reporting__account_report.sql
@@ -25,7 +25,7 @@ aggregated as (
         sum(clicks) as clicks,
         sum(impressions) as impressions,
         sum(spend) as spend,
-        sum(conversions) as conversions,
+        sum(conversions) as conversions
         sum(conversions_value) as conversions_value
 
         {{ ad_reporting_persist_pass_through_columns(pass_through_variable='ad_reporting__account_passthrough_metrics', transform = 'sum', alias_fields=['conversions', 'conversions_value']) }}
@@ -49,7 +49,8 @@ source_relation
 ,sum(clicks) as clicks
 ,sum(impressions) as impressions
 ,sum(spend) as spend
-,sum(conversions) as conversions    
+,sum(conversions) as conversions
+,sum(conversions_value) as conversions_value    
 from 
     {{ref('youtube_ads__custom_ad_summary_report')}}
 group by 1,2,3,4,5
