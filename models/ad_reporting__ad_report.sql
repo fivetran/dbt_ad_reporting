21ff0acdb2b8c1af2973167b484cbc066bcadb3b
atulkr-mbm <atul.krishnan@mediabymother.com>
2025-09-23 17:02:12 +0530

    zefr changes - ad reporting

--- models/ad_reporting__ad_report.sql
+++ models/ad_reporting__ad_report.sql
@@ -95,6 +95,32 @@ sum(player_75p_complete) as video_75p_watched,
 sum(player_completed_views) as video_complete_watched
 from {{ ref('ttd_ads__ad_report') }}  
 group by 1,2,3,4,5,6,7,8,9,10,11
+
+union all
+
+SELECT 
+'' as source_relation
+,performance_date as date_day
+,'zefr' as platform
+,null as account_id
+,null as account_name
+,campaign_group_id as campaign_id
+,campaign_group_name as campaign_name
+,ad_group_id
+,ad_group_name
+,null as ad_id
+,ad_name
+,sum(clicks) as clicks
+,sum(impressions) as impressions
+,sum(spend) as spend
+, null as conversions 
+, null as conversions_value,
+sum(video_view_25pct) as video_25p_watched, 
+sum(video_view_50pct) as video_50p_watched, 
+sum(video_view_75pct) as video_75p_watched, 
+sum(video_view_100pct) as video_complete_watched
+from {{ ref('zefr_ad_summary_report.sql') }}  
+group by 1,2,3,4,5,6,7,8,9,10,11
 )
 
 select *
