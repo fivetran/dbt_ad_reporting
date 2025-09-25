414bc14cc38b0fd2db5c7998e9e5d52a79cc3775
atulkr-mbm <atul.krishnan@mediabymother.com>
2025-09-25 11:20:43 +0530

    Update ad_reporting__ad_group_report.sql

--- models/ad_reporting__ad_group_report.sql
+++ models/ad_reporting__ad_group_report.sql
@@ -103,17 +103,16 @@ SELECT
 ,campaign_group_name as campaign_name
 ,ad_group_id
 ,ad_group_name
-,sum(clicks) as clicks
-,sum(impressions) as impressions
-,sum(spend) as spend
+,clicks as clicks
+,impressions as impressions
+,spend as spend
 , null as conversions 
 , null as conversions_value,
-sum(video_view_25pct) as video_25p_watched, 
-sum(video_view_50pct) as video_50p_watched, 
-sum(video_view_75pct) as video_75p_watched, 
-sum(video_view_100pct) as video_complete_watched
-from {{ ref('zefr_adgroup_summary_report.sql') }}
-group by 1,2,3,4,5,6,7,8,9
+video_view_25pct as video_25p_watched, 
+video_view_50pct as video_50p_watched, 
+video_view_75pct as video_75p_watched, 
+video_view_100pct as video_complete_watched
+from {{ ref('zefr_adgroup_summary_report') }}
 )
 
 select *
